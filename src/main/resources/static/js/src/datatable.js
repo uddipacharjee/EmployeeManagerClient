d707de02b57018651230c08dec3ae9d6f1eeda05
uddipacharjee <bsse0730@iit.du.ac.bd>
2020-03-11 15:13:38 +0600

    search box added

--- src/main/resources/static/js/src/datatable.js
+++ src/main/resources/static/js/src/datatable.js
@@ -1,39 +1,90 @@
 $(document).ready( function () {
     var table = $('#employeesTable').DataTable({
+        //"dom": '<"top"i>rt<"bottom"><"clear">',
+        //"dom": 'frtip',
+        //"sDom": '<"top">l<"container" t>ip',
+        "sDom": 'ltrip',//remove f
+        //"select" : 'true',
         "sAjaxSource": "/rest/employees",
         "sAjaxDataProp": "",
-        "order": [[ 0, "asc" ]],
+        "order": [[0, "asc"]],
         "aoColumns": [
-            { "mData": "name" },
-            { "mData": "title" },
-            { "mData": "email" },
-            { "mData": "teamName" },
-            { "mData": "joinedDate" },
-            { "mData": "mobile" },
-            { "mData": "id"}
+            {"mData": "name"},
+            {"mData": "title"},
+            {"mData": "email"},
+            {"mData": "teamName"},
+            {"mData": "joinedDate"},
+            {"mData": "mobile"},
+            {"mData": "id"}
         ],
-        "aoColumnDefs":[
+        "aoColumnDefs": [
             {
-                "aTargets":[-1],
-                "mRender":function(data){
-                    return '<a type="button" class="btn btn-primary" href="/update?id='+data+'">Edit</a>' +
-                        '<a type="button" class="btn btn-warning" href="/delete?id='+data+'">Delete</a>';
+                "aTargets": [-1],
+                "mRender": function (data) {
+                    return '<a type="button" class="btn btn-primary" href="/update?id=' + data + '">Edit</a>' +
+                        '<a type="button" class="btn btn-warning" href="/delete?id=' + data + '">Delete</a>';
                 }
             },
             {
-                "aTargets":[-3],
-                "mRender":function(data){
+                "aTargets": [-3],
+                "mRender": function (data) {
                     const d = new Date(data);
-                    const ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(d);
-                    const mo = new Intl.DateTimeFormat('en', { month: 'short' }).format(d);
-                    const da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(d);
+                    const ye = new Intl.DateTimeFormat('en', {year: 'numeric'}).format(d);
+                    const mo = new Intl.DateTimeFormat('en', {month: 'short'}).format(d);
+                    const da = new Intl.DateTimeFormat('en', {day: '2-digit'}).format(d);
 
                     return `${da}-${mo}-${ye}`;
                 }
             }
 
-            ]
+        ]
 
 
-    })
+    });
+    /*
+    table.columns().every( function () {
+        let that = this;
+
+        $('#nameSearch' ).on( 'keyup change clear', function () {
+            if ( that.search() !== this.value ) {
+                that
+                    .search( this.value )
+                    .draw();
+            }
+        } );
+    } );
+    */
+    /*
+    $('#nameSearch').on('keyup click',function () {
+        if ( table.columns().search() !== this.value ) {
+            that
+                .search( this.value )
+                .draw();
+        }
+    });
+    console.log("hello");
+
+     */
+    /*
+        $('#nameSearch').on( 'keyup click', function () {
+            //table.search($('#nameSearch').val()).draw();
+            var x=table.column;
+            document.getElementById("test").innerHTML=x;
+        } );
+
+     */
+    $("#nameSearch").on('keyup click',function () {
+        table.columns(0).search(this.value).draw();
+    });
+
+    $("#emailSearch").on('keyup click',function () {
+        table.columns(2).search(this.value).draw();
+    });
+
+    $("#teamSearch").on('keyup click',function () {
+        table.columns(3).search(this.value).draw();
+    });
+    $("#mobileSearch").on('keyup click',function () {
+        table.columns(5).search(this.value).draw();
+    });
 });
--- src/main/webapp/WEB-INF/views/list-of-employee.jsp
+++ src/main/webapp/WEB-INF/views/list-of-employee.jsp
@@ -3,17 +3,50 @@
     <%@ page isELIgnored="false" %>
     <meta charset="utf-8" />
     <title>Spring Boot + JPA + Datatables</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/jquery.dataTables.min.css">
     <script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
     <script src="/js/src/datatable.js"></script>
+    <link rel="stylesheet" href="/css/src/design.css">
 </head>
 
 <body>
 
 <H1>Employee List</H1>
+<div class="container" id="searchBox">
+    <div class="col-xs-6">
+        <div class="input-group">
+            <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+            <input type="text" id="nameSearch" class="form-control" placeholder="name.." >
+        </div>
+    </div>
+    <div class="col-xs-6">
+        <div class="input-group">
+            <span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span>
+            <input type="text" id="emailSearch" class="form-control" placeholder="email.." >
+        </div>
+    </div>
+
+
+
+    <div class="col-xs-6">
+        <div class="input-group">
+            <span class="input-group-addon"><i class="glyphicon glyphicon-search"></i></span>
+            <input type="text" id="teamSearch" class="form-control" placeholder="team.." >
+        </div>
+    </div>
+
+    <div class="col-xs-6">
+        <div class="input-group">
+            <span class="input-group-addon"><i class="glyphicon glyphicon-phone"></i></span>
+            <input type="text" id="mobileSearch" class="form-control" placeholder="mobile.." >
+        </div>
+    </div>
+</div>
+
+<p id="test"></p>
 <div class="container">
     <table id="employeesTable" class="table table-striped">
         <thead>
